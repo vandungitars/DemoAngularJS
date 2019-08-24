146fee13f2c1c9a659223069c7f8c137c78e6914
dung-lv <dung-lv@trente.asia>
2019-08-24 14:08:57 +0700

    push done project demo angularjs

---
+++ custom.js
@@ -0,0 +1,28 @@
+﻿ var app = angular.module('myApp',['ngMaterial']);
+ app.controller('MyController',  function($scope){
+
+  var humans = [
+	  {
+	  	name: "Dung",
+	  	age: 21,
+	  	gender: "male"
+	  },
+	  {
+	  	name: "Huong",
+	  	age: 21,
+	  	gender: "famemale"
+	  },
+	  {
+	  	name: "Linh",
+	  	age: 21,
+	  	gender: "famemale"
+	  },
+	  {
+	  	name: "Viet",
+	  	age: 21,
+	  	gender: "male"
+	  }
+	]
+  $scope.humans = humans;
+  $scope.searchText = '';
+ })
